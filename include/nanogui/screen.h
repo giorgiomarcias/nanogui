3bdd6e5d4d1d85a9907b1d0ed80a9ee27bf75edf
Giorgio Marcias <marcias.giorgio@gmail.com>
2015-12-28 19:28:57 -0800

    Suppressed warning

--- include/nanogui/screen.h
+++ include/nanogui/screen.h
@@ -69,6 +69,8 @@ public:
     void setShutdownGLFWOnDestruct(bool v) { mShutdownGLFWOnDestruct = v; }
     bool shutdownGLFWOnDestruct() { return mShutdownGLFWOnDestruct; }
 
+    using ScreenCore::performLayout;
+    
     /// Compute the layout of all widgets
     void performLayout() {
         Widget::performLayout(mNVGContext);
